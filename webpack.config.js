99df2ddd1b3d077efbdd8fd7a51a4245f1f09ba6
noonworks <noonworks@gmail.com>
2019-10-08 18:50:49 +0900

    Fix bundled library build scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,23 @@ module.exports = (env, argv) => {
   const buildLib = env && env.lib;
 
   const entry = buildLib ? './src/l2d/index.ts' : './src/a-l2d.ts';
-  const outName = buildLib ? 'a-l2d-lib.js' : 'a-l2d.js';
-  const outDir = buildLib ? '/dist' : '/docs/js';
+  const output =
+    buildLib ? {
+      path: `${__dirname}/dist`,
+      filename: 'a-l2d-lib.js',
+      library: 'AL2D',
+      libraryTarget: 'var'
+    } : {
+      path: `${__dirname}/docs/js`,
+      filename: 'a-l2d.js'
+    };
   const config = buildLib ? 'tsconfig.lib.json' : 'tsconfig.json';
 
   return {
     devtool: devMode ? 'nosources-source-map' : 'none',
 
     entry: entry,
-    output: {
-      path: `${__dirname}` + outDir,
-      filename: outName
-    },
+    output: output,
     module: {
       rules: [
         {
